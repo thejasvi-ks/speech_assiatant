43fd26cddbf034e86a8d82677fff759c9673925e
Thejasvi K S <tks@stats.com>
2019-12-27 12:46:28 +0530

    Changed assistant to Edith.

--- speech_assist.py
+++ speech_assist.py
@@ -12,18 +12,18 @@ r = sr.Recognizer()
 def record_audio(ask = False):
     with sr.Microphone() as source:
         if ask:
-            alexis_speak(ask)
+            edith_speaks(ask)
         audio = r.listen(source)
         voice_data = ''
         try:
             voice_data = r.recognize_google(audio)
         except sr.UnknownValueError:
-            alexis_speak('Sorry, I did not get that')
+            edith_speaks('Sorry, I did not get that')
         except sr.RequestError:
-            alexis_speak('Sorry, My Speech service is Down')
+            edith_speaks('Sorry, My Speech service is Down')
         return voice_data
 
-def alexis_speak(audio_string):
+def edith_speaks(audio_string):
     tts = gTTS(text=audio_string, lang='en')
     r = random.randint(1,10000000)
     audio_file = 'audio-' + str(r) + '.mp3'
@@ -36,22 +36,23 @@ def respond(voice_data):
     if 'what is your name' in voice_data:
         alexis_speak('My name is Edith.')
     if 'what time is it' in voice_data:
-        alexis_speak(ctime())
+        edith_speaks(ctime())
     if 'search' in voice_data:
         search = record_audio('What do you want to search for?')
         url = 'https://google.com/search?q=' + search
         webbrowser.get().open(url)
-        alexis_speak('Here is what I found for ' + search)
+        edith_speaks('Here is what I found for ' + search)
     if 'find location' in voice_data:
         location = record_audio('What is the location?')
         url = 'https://google.nl/maps/place/' + location + '/&amp;'
         webbrowser.get().open(url)
         alexis_speak('Here is the location of ' + location)
     if 'exit' in voice_data:
+        edith_speaks('See you later Boss.')
         exit()
 
 time.sleep(1)
-alexis_speak('How can I help you?')
+edith_speaks('How can I help you?')
 while 1:
     voice_data = record_audio()
     respond(voice_data)
